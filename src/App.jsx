5b12fbe0387baad3798077fb24602658377f4933
Gordon Adanyi <145518427+gordonadanyi@users.noreply.github.com>
2026-06-22 19:14:12 +0100

    Refactor App component to include routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import SpinWheel from "./components/Wheel";
+import ResultPage from "./components/ResultPage";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 flex items-center justify-center">
-      <div className=" ">
-        {/* <h1 className="text-3xl font-bold text-center mb-6 text-gray-800">Spin the Wheel!</h1>
-        <p className="text-center text-gray-600 mb-8">Try your luck and win exciting prizes!</p> */}
-        <SpinWheel />
-      </div>
+    <div
+      className="relative min-h-screen flex items-center justify-center w-full bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url('/homeBg.png')` }}
+    >
+      <Routes>
+        <Route path="/" element={<SpinWheel />} />
+        <Route path="/result" element={<ResultPage />} />
+      </Routes>
     </div>
   );
 }
